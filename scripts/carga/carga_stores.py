2054702d71db5ecd9f8b547ad2084a9e2f913d8e
Said Quinto <ariel.quinto@epn.edu.ec>
2026-07-07 05:00:58 +0000

    Hago carga de stores y EDA inicial del mismo

---
+++ scripts/carga/carga_stores.py
@@ -0,0 +1,45 @@
+import polars as pl
+import json
+import os
+
+def cargar_stores():
+    df = pl.read_csv("data/datasets/stores.csv")
+    df.write_parquet("data/processed/stores.parquet")
+    print(f"stores cargado: {df.shape}")
+
+
+def diagnosticar_stores():
+    df = pl.read_parquet("data/processed/stores.parquet")
+
+    print(f"Forma del dataset: \n{df.shape}")
+    print(f"Tipo de datos: \n{df.dtypes}")
+    print(f"Descripcion de datos: \n{df.describe()}")
+    print(f"Nombre de columnas: \n{df.columns}")
+    print(f"Total de nulos por columna: \n{df.null_count()}")
+    print(f"Total de filas duplicadas: \n{df.is_duplicated().sum()}")
+
+    info_stores = {
+        "filas": df.shape[0],
+        "columnas": df.shape[1],
+        "tipos_de_dato": {col: str(tipo) for col, tipo in df.schema.items()},
+        "nulos_por_columna": df.null_count().to_dicts()[0],
+        "duplicados": int(df.is_duplicated().sum())
+    }
+
+    RUTA_REPORTE = "data/reports/reporte_informativo.json"
+
+
+    if os.path.exists(RUTA_REPORTE):
+        with open(RUTA_REPORTE, "r", encoding= "utf-8") as archivo:
+            reporte = json.load(archivo)
+    else:
+        reporte = {}
+
+    reporte["INFORMACION_STORES"] = info_stores
+
+    with open (RUTA_REPORTE, "w", encoding="utf-8") as archivo:
+        json.dump(reporte, archivo, indent=4, ensure_ascii=False)
+
+
+cargar_stores()
+diagnosticar_stores()
