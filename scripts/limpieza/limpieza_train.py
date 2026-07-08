f9a7e0e8bda2dca4690d91bc9cdddcddacd3a66b
Said Quinto <ariel.quinto@epn.edu.ec>
2026-07-08 05:20:11 +0000

    Conversion de columnas fecha: string -> date

--- scripts/limpieza/limpieza_train.py
+++ scripts/limpieza/limpieza_train.py
@@ -1,17 +1,34 @@
 import polars as pl
-import json
-import os
 
 def limpiar_train():
     df = pl.read_parquet("Analisis_de_Datos_Proyecto/data/processed/train.parquet")
-    print("Cambiando nombre de columnas...")
-    print("Ingles -> Español")
-    df = df.rename({"date":"fecha",
-                    "store_nbr":"num_tienda",
-                    "family":"categoria",
-                    "sales":"ventas",
-                    "onpromotion":"promocion"})
-    print("Columnas cambiadas con exito")
-    print("Nombres nuevos: ", df.columns)
+
+    print("Renombrando columnas...")
+    df = df.rename({
+        "date": "fecha",
+        "store_nbr": "num_tienda",
+        "family": "categoria",
+        "sales": "ventas",
+        "onpromotion": "promocion",
+    })
+    print("Columnas renombradas correctamente.")
+
+    print("Conversion de tipos de datos...")
+
+    # Transformar a tipo fecha
+    df = df.with_columns(
+        pl.col("fecha").str.to_datetime(format="%Y-%m-%d")
+    )
+
+    # Quitar los milisegundos
+    df = df.with_columns(
+        pl.col("fecha").dt.date()
+    )
+    print("Conversión completada.")
+
+    print("\nColumnas y tipos")
+    for columna, tipo in df.schema.items():
+        print(f"- {columna}: {tipo}")
+
 
 limpiar_train()
