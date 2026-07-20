fb264e3263dd2d1ce0beae60d718da42ce42e88c
Said Quinto <ariel.quinto@epn.edu.ec>
2026-07-20 15:22:40 +0000

    Correccion de la url de conexion a PostgreSQL

--- scripts/exportar/exportacion.py
+++ scripts/exportar/exportacion.py
@@ -1,6 +1,6 @@
 import polars as pl
 
-conn_str = "postgresql://admin:admin123@localhost:5432/proyecto_favorita"
+conn_str = "postgresql://favorita_user:183132@localhost:5432/favorita_db"
 df = pl.read_parquet("/home/azureuser/proyecto_favorita/Analisis_de_Datos_Proyecto/data/processed/consolidacion.parquet")
 
 df.write_database(
